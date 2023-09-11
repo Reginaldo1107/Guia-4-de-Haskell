14eb8fafa1912394ef788303cdc7117d10424295
Reginaldo1107 <reginaldohinojosabaldera@gmail.com>
2023-09-11 20:00:59 -0300

    Agrego el b ejercicio16.hs

--- ejercicio16.hs
+++ ejercicio16.hs
@@ -13,3 +13,14 @@ buscoMenorConsecutivo n m |mod n m == 0 = m
 menorDivisor:: Integer ->Integer
 menorDivisor x = buscoMenorConsecutivo x 2 
  
+ --(b) Implementar la funcion 
+-- esPrimo :: Integer ->Bool  que indica si un numero 
+--natural pasado como parametro es primo
+buscoPrimoConsecutivo :: Integer -> Integer -> Bool
+buscoPrimoConsecutivo x y | x == y = True 
+                          |mod x y == 0 = False 
+                          | otherwise = buscoPrimoConsecutivo x (y+1)
+esPrimo :: Integer ->Bool
+esPrimo x =  buscoPrimoConsecutivo x 2
+
+
