8fe6086c98b20ac0d19161b400518c9766d9addb
Reginaldo1107 <reginaldohinojosabaldera@gmail.com>
2023-09-11 10:17:09 -0300

    Arregle el problema de pasar de Entero a Float

--- ejercicio11.hs
+++ ejercicio11.hs
@@ -1,8 +1,8 @@
-PERO LO HICE PONIENDO TODOS FLOAT 
+
 factorial :: Float -> Float
 factorial x | x == 1 = 1.0
             |otherwise =  x * factorial (x -1)
 
-eAprox :: Float -> Float
-eAprox x | x == 0  = 1 
-         |otherwise = 1.0/factorial x + eAprox (x-1)
+eAprox :: Int -> Float
+eAprox x | x == 0  = 1
+         |otherwise = 1/factorial (fromIntegral x ) + eAprox (x-1)
